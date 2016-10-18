69427909b50d6a3b0d0feab26f446a9b72859a14
Leon Eckert <eckertleon@gmail.com>
2016-10-17 22:06:22 -0400

    local server backforth

--- extension/popup.js
+++ extension/popup.js
@@ -5,7 +5,7 @@ function start(FCsecretID, FCpeerID, FCusername){
     console.log("FCpeerID", FCpeerID);
     console.log("FCusername", FCusername);
 
-    // var friends = {"pending": [], "confirmed": {}};
+    var FCfriends = {"pending": [], "confirmed": {}};
 
     function toServer(postkey, data, callback){   
         var xmlhttp = new XMLHttpRequest();       
@@ -20,107 +20,107 @@ function start(FCsecretID, FCpeerID, FCusername){
         xmlhttp.send(JSON.stringify(toSend));
     }
 
-    // function addFriend(){
-    //     document.getElementById("friend_not_exist_notification").innerHTML = "";
-    //     var friendToAdd = document.getElementById("friendText").value;
-    //     console.log(username,"wants to add", friendToAdd,"as a friend.");
-    //     toServer("addfriend", {"name": friendToAdd}, function(response){
-    //         var res = JSON.parse(response);
+    function addFriend(){
+        document.getElementById("friend_not_exist_notification").innerHTML = "";
+        var friendToAdd = document.getElementById("friendText").value;
+        console.log(username,"wants to add", friendToAdd,"as a friend.");
+        toServer("addfriend", {"name": friendToAdd}, function(response){
+            var res = JSON.parse(response);
             
-    //         if(res["status"] === "pending"){
-    //             friends["pending"].push(friendToAdd);
-    //         }else if(res["status"] === "friends"){
-    //             friends["confirmed"][friendToAdd] = res["friendID"];
-    //         }
-    //         renderPopup();
+            if(res["status"] === "pending"){
+                friends["pending"].push(friendToAdd);
+            }else if(res["status"] === "friends"){
+                friends["confirmed"][friendToAdd] = res["friendID"];
+            }
+            renderPopup();
             
-    //         if(res["status"] === "unavailable"){
-    //             document.getElementById("friend_not_exist_notification").innerHTML = "<b>"+friendToAdd+"</b> could not be found.";
-    //         }
-    //     })
-    // }
+            if(res["status"] === "unavailable"){
+                document.getElementById("friend_not_exist_notification").innerHTML = "<b>"+friendToAdd+"</b> could not be found.";
+            }
+        })
+    }
 
-    // function sendStream(recipient) {   
-    //     chrome.runtime.sendMessage({"myID": myID, "nameToCall": recipient, "idToCall": friends["confirmed"][recipient]});
-    // }
+    function sendStream(recipient) {   
+        chrome.runtime.sendMessage({"myID": myID, "nameToCall": recipient, "idToCall": friends["confirmed"][recipient]});
+    }
 
-    // function renderPopup(){
-    //     document.body.innerHTML = "";
+    function renderPopup(){
+        document.body.innerHTML = "";
         
-    //     // include welcome
-    //     var welcome = document.createElement("h3");
-    //     welcome.innerHTML = "Hello, " + username + "!";
-    //     document.body.appendChild(welcome);
+        // include welcome
+        var welcome = document.createElement("h3");
+        welcome.innerHTML = "Hello, " + username + "!";
+        document.body.appendChild(welcome);
 
-    //     //friends
-    //     var friendsWrapper = document.createElement("div");
-    //     friendsWrapper.id = "friendsWrapper";
+        //friends
+        var friendsWrapper = document.createElement("div");
+        friendsWrapper.id = "friendsWrapper";
 
-    //     var confirmed = Object.keys(friends["confirmed"]);
-    //     if(confirmed.length > 0){
-    //         var confirmedFriends = document.createElement("div");
-    //         confirmedFriends.id = "confirmedFriends";
-    //         confirmedFriends.innerHTML = "<p><b>Your friends:</b></p>";
+        var confirmed = Object.keys(friends["confirmed"]);
+        if(confirmed.length > 0){
+            var confirmedFriends = document.createElement("div");
+            confirmedFriends.id = "confirmedFriends";
+            confirmedFriends.innerHTML = "<p><b>Your friends:</b></p>";
             
-    //         for(var i = 0; i < confirmed.length; i++){
+            for(var i = 0; i < confirmed.length; i++){
                 
-    //             var friendListing = document.createElement("p");
-    //             friendListing.innerHTML = ">> "+confirmed[i]+" ";
+                var friendListing = document.createElement("p");
+                friendListing.innerHTML = ">> "+confirmed[i]+" ";
 
-    //             var callButton = document.createElement("a");
-    //             callButton.innerHTML = "go live";
-    //             callButton.href = "#";
-    //             callButton.id = confirmed[i];
-    //             callButton.addEventListener('click', function(){
-    //                 sendStream(this.id);
-    //             })
+                var callButton = document.createElement("a");
+                callButton.innerHTML = "go live";
+                callButton.href = "#";
+                callButton.id = confirmed[i];
+                callButton.addEventListener('click', function(){
+                    sendStream(this.id);
+                })
 
-    //             friendListing.appendChild(callButton);
-    //             confirmedFriends.appendChild(friendListing);
-    //         }
-    //         friendsWrapper.appendChild(confirmedFriends);
-    //     }
+                friendListing.appendChild(callButton);
+                confirmedFriends.appendChild(friendListing);
+            }
+            friendsWrapper.appendChild(confirmedFriends);
+        }
 
 
-    //     var pending = friends["pending"];
-    //     if(pending.length > 0){
-    //         var pendingFriends = document.createElement("div");
-    //         pendingFriends.id = "pendingFriends";
-    //         pendingFriends.innerHTML = "<p><b>Added:</b> (pending until they add you, too)</p>";
-    //         for(var i = 0; i < pending.length; i++){
-    //             pendingFriends.innerHTML += "<p>"+pending[i]+"</p>";
-    //         }
-    //         friendsWrapper.appendChild(pendingFriends);
-    //     }
-    //     document.body.appendChild(friendsWrapper);
+        var pending = friends["pending"];
+        if(pending.length > 0){
+            var pendingFriends = document.createElement("div");
+            pendingFriends.id = "pendingFriends";
+            pendingFriends.innerHTML = "<p><b>Added:</b> (pending until they add you, too)</p>";
+            for(var i = 0; i < pending.length; i++){
+                pendingFriends.innerHTML += "<p>"+pending[i]+"</p>";
+            }
+            friendsWrapper.appendChild(pendingFriends);
+        }
+        document.body.appendChild(friendsWrapper);
 
-    //     //friend adder
-    //     var newFriendsWrapper = document.createElement("div");
-    //     newFriendsWrapper.style.borderTop = "thin solid #000000";
-    //     newFriendsWrapper.style.marginTop = "8px";
-    //     newFriendsWrapper.style.paddingTop = "8px";
-    //     newFriendsWrapper.id = "newFriendsWrapper";
+        //friend adder
+        var newFriendsWrapper = document.createElement("div");
+        newFriendsWrapper.style.borderTop = "thin solid #000000";
+        newFriendsWrapper.style.marginTop = "8px";
+        newFriendsWrapper.style.paddingTop = "8px";
+        newFriendsWrapper.id = "newFriendsWrapper";
 
-    //     var friendText = document.createElement("input");
-    //     friendText.type = "text";
-    //     friendText.value = "Add a friend.";
-    //     friendText.id = "friendText";
-    //     newFriendsWrapper.appendChild(friendText);
+        var friendText = document.createElement("input");
+        friendText.type = "text";
+        friendText.value = "Add a friend.";
+        friendText.id = "friendText";
+        newFriendsWrapper.appendChild(friendText);
         
-    //     var friendSubmit = document.createElement("input");
-    //     friendSubmit.type = "button";
-    //     friendSubmit.value = "Add friend";
-    //     friendSubmit.id = "friendSubmit";
-    //     friendSubmit.addEventListener("click", addFriend);
-    //     newFriendsWrapper.appendChild(friendSubmit);
+        var friendSubmit = document.createElement("input");
+        friendSubmit.type = "button";
+        friendSubmit.value = "Add friend";
+        friendSubmit.id = "friendSubmit";
+        friendSubmit.addEventListener("click", addFriend);
+        newFriendsWrapper.appendChild(friendSubmit);
         
-    //     var friendNotExistNotification = document.createElement("p");
-    //     friendNotExistNotification.id = "friend_not_exist_notification";
-    //     newFriendsWrapper.appendChild(friendNotExistNotification);
-    //     document.body.appendChild(newFriendsWrapper);
+        var friendNotExistNotification = document.createElement("p");
+        friendNotExistNotification.id = "friend_not_exist_notification";
+        newFriendsWrapper.appendChild(friendNotExistNotification);
+        document.body.appendChild(newFriendsWrapper);
 
 
-    // }
+    }
 
     /////////////////////////// RUTIME ///////////////////////////
 
@@ -129,8 +129,9 @@ function start(FCsecretID, FCpeerID, FCusername){
     toServer("hello", {}, function(response){
         // console.log("GOT MY FRIEND OBJ");
         console.dir(response);
-        // friends = JSON.parse(response);
-        // renderPopup();
+        FCfriends = JSON.parse(response);
+        console.dir(FCfriends);
+        renderPopup();
     });
 
 
